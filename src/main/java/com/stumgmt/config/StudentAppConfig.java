e5a333b77ec2c741b00b9f5c28e1c26e83539dac
Aman Singh Tomar <amansinghtomar2209@gmail.com>
2022-01-28 03:46:14 +0530

    MySQL is configured and working

--- src/main/java/com/stumgmt/config/StudentAppConfig.java
+++ src/main/java/com/stumgmt/config/StudentAppConfig.java
@@ -1,8 +1,12 @@
 package com.stumgmt.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -11,11 +15,29 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages = { "com.stumgmt" })
 public class StudentAppConfig {
 
+	//Configuring View Resolver
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+
+	//Configurin MySQL Database
+	@Bean
+	public DataSource datasource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/studentmanagement");
+		return dataSource;
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource());
+		return jdbcTemplate;
 	}
 }
---
+++ src/main/java/com/stumgmt/dao/StudentDAO.java
@@ -0,0 +1,10 @@
+package com.stumgmt.dao;
+
+import java.util.List;
+
+import com.stumgmt.dto.StudentsDTO;
+
+public interface StudentDAO {
+
+	List<StudentsDTO>loadStudent();
+}
