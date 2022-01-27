a21fdd67148adce453c5cad7bdb8603f987494c5
Aman Singh Tomar <amansinghtomar2209@gmail.com>
2022-01-28 01:45:03 +0530

    Front Controller Configured

---
+++ src/main/java/com/stumgmt/config/StudentAppConfig.java
@@ -0,0 +1,13 @@
+package com.stumgmt.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configurable
+@EnableWebMvc
+@ComponentScan(basePackages ={"com.stumgmt"})
+public class StudentAppConfig { 
+
+	
+}
---
+++ src/main/java/com/stumgmt/ctl/StudentController.java
@@ -0,0 +1,19 @@
+package com.stumgmt.ctl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class StudentController {
+
+// both are same or equivalent	
+//	@RequestMapping(value="/showStudent", method=RequestMethod.GET)
+	@GetMapping("/showStudent")
+	public String showStudentList() {
+		
+		return "student-list";
+	}
+	
+}
